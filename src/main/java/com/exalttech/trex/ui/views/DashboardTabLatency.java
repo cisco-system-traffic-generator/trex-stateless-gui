03657582223fd5a39d8951affadaaa93e412413f
Pavel Agapov <pavel.agapov@xored.com>
2017-03-10 16:59:21 +0700

    Add latency chart to the latency dashboard.

--- src/main/java/com/exalttech/trex/ui/views/DashboardTabLatency.java
+++ src/main/java/com/exalttech/trex/ui/views/DashboardTabLatency.java
@@ -1,12 +1,69 @@
 package com.exalttech.trex.ui.views;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import com.exalttech.trex.util.Initialization;
 
 
 public class DashboardTabLatency extends AnchorPane {
+    private static final List<String> chartTypes = new ArrayList<String>() {{
+        add("Histogram");
+        add("Chart");
+    }};
+    private static final List<String> latencyIntervals = new ArrayList<String>() {{
+        add("60");
+        add("90");
+        add("120");
+        add("300");
+    }};
+
+    @FXML
+    private DashboardTabLatencyHistogram histogram;
+    @FXML
+    private DashboardTabLatencyChart chart;
+    @FXML
+    private ComboBox chartTypeComboBox;
+    @FXML
+    private ComboBox intervalComboBox;
+
     public DashboardTabLatency() {
         Initialization.initializeFXML(this, "/fxml/Dashboard/tabs/latency/DashboardTabLatency.fxml");
+        initializeChartTypeComboBox();
+        initializeIntervalComboBox();
+    }
+
+    private void initializeChartTypeComboBox() {
+        chartTypeComboBox.getItems().addAll(FXCollections.observableArrayList(chartTypes));
+        chartTypeComboBox.valueProperty().addListener(new ChangeListener<String>() {
+            public void changed(ObservableValue observable, String oldValue, String newValue) {
+                histogram.setVisible(newValue.equals("Histogram"));
+                setChartVisibility(newValue.equals("Chart"));
+            }
+        });
+        chartTypeComboBox.setValue("Histogram");
+    }
+
+    private void setChartVisibility(boolean isVisible) {
+        chart.setVisible(isVisible);
+        intervalComboBox.setDisable(!isVisible);
+    }
+
+    private void initializeIntervalComboBox() {
+        intervalComboBox.getItems().addAll(FXCollections.observableArrayList(latencyIntervals));
+        intervalComboBox.valueProperty().addListener(new ChangeListener<String>() {
+            public void changed(ObservableValue observable, String oldValue, String newValue) {
+                chart.setInterval(Integer.parseInt(newValue));
+            }
+        });
+        intervalComboBox.setValue("60");
     }
 }
---
+++ src/main/java/com/exalttech/trex/util/ArrayHistory.java
@@ -0,0 +1,41 @@
+package com.exalttech.trex.util;
+
+import java.util.function.Consumer;
+
+
+public class ArrayHistory<T> {
+    private Object[] history;
+    private int historyLength;
+    private int currentLast;
+    private int capacity;
+
+    public ArrayHistory(int historyLength) {
+        this.historyLength = historyLength;
+        history = new Object[this.historyLength];
+        currentLast = 0;
+        capacity = 0;
+    }
+
+    public void add(T item) {
+        if (currentLast >= historyLength) {
+            currentLast = 0;
+        }
+        history[currentLast++] = item;
+        if (capacity < historyLength) {
+            capacity++;
+        }
+    }
+
+    public void forEach(Consumer<? super T> action) {
+        for (int i = capacity, current = currentLast; i > 0; i--) {
+            if (current >= capacity) {
+                current = 0;
+            }
+            action.accept((T) history[current++]);
+        }
+    }
+
+    public int size() {
+        return capacity;
+    }
+}
