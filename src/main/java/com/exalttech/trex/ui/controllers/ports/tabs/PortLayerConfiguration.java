aba87db95dfd3ee2738972ad13910cb9c6e65470
Vyacheslav Ogai <vyacheslav.ogai@gmail.com>
2017-03-14 14:12:37 +0700

    Add view for PortLayerConfiguration component

--- src/main/java/com/exalttech/trex/ui/controllers/ports/tabs/PortLayerConfiguration.java
+++ src/main/java/com/exalttech/trex/ui/controllers/ports/tabs/PortLayerConfiguration.java
@@ -1,9 +1,15 @@
 package com.exalttech.trex.ui.controllers.ports.tabs;
 
+import com.exalttech.trex.util.Initialization;
 import javafx.fxml.FXML;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
 
-public class PortLayerConfiguration extends AnchorPane {
+public class PortLayerConfiguration extends BorderPane {
     @FXML
     private AnchorPane root;
+
+    public PortLayerConfiguration() {
+        Initialization.initializeFXML(this, "/fxml/ports/PortLayerConfiguration.fxml");
+    }
 }
