fc7adcb443138cd34cb5a1cba80fc0c0c8760fe3
Vyacheslav Ogai <vyacheslav.ogai@gmail.com>
2017-03-14 15:56:11 +0700

    Add UI controls for PortHardwareCounters

--- src/main/java/com/exalttech/trex/ui/controllers/ports/tabs/PortHardwareCounters.java
+++ src/main/java/com/exalttech/trex/ui/controllers/ports/tabs/PortHardwareCounters.java
@@ -1,9 +1,10 @@
 package com.exalttech.trex.ui.controllers.ports.tabs;
 
-import javafx.fxml.FXML;
-import javafx.scene.layout.AnchorPane;
+import com.exalttech.trex.util.Initialization;
+import javafx.scene.layout.BorderPane;
 
-public class PortHardwareCounters extends AnchorPane {
-    @FXML
-    private AnchorPane root;
+public class PortHardwareCounters extends BorderPane {
+    public PortHardwareCounters() {
+        Initialization.initializeFXML(this, "/fxml/ports/PortHardwareCounters.fxml");
+    }
 }
