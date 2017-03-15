bd8e2357f60f17fc84271f5e04426fed30cb76ab
Vyacheslav Ogai <vyacheslav.ogai@gmail.com>
2017-03-15 15:53:08 +0700

    Inject port components to parent component.

--- src/main/java/com/exalttech/trex/ui/controllers/ports/PortView.java
+++ src/main/java/com/exalttech/trex/ui/controllers/ports/PortView.java
@@ -1,9 +1,22 @@
 package com.exalttech.trex.ui.controllers.ports;
 
+import com.exalttech.trex.ui.controllers.ports.tabs.PortAttributes;
+import com.exalttech.trex.ui.controllers.ports.tabs.PortHardwareCounters;
+import com.exalttech.trex.ui.controllers.ports.tabs.PortLayerConfiguration;
 import com.exalttech.trex.util.Initialization;
+import javafx.fxml.FXML;
 import javafx.scene.control.TabPane;
 
 public class PortView extends TabPane {
+    
+    @FXML
+    PortAttributes portAttributes;
+    
+    @FXML
+    PortLayerConfiguration layerConfig;
+    
+    @FXML
+    PortHardwareCounters hardwareCounters;
     
     public PortView() {
         Initialization.initializeFXML(this, "/fxml/ports/Port.fxml");
