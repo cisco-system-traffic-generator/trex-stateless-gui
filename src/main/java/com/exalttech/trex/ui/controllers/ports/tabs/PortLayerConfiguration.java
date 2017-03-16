358fc70181165fc9ce6c5d529c5277d6aa9d8113
Vyacheslav Ogai <vyacheslav.ogai@gmail.com>
2017-03-16 20:59:57 +0700

    Create text field for src and dst for each layer type. Add bindings

--- src/main/java/com/exalttech/trex/ui/controllers/ports/tabs/PortLayerConfiguration.java
+++ src/main/java/com/exalttech/trex/ui/controllers/ports/tabs/PortLayerConfiguration.java
@@ -3,63 +3,86 @@ package com.exalttech.trex.ui.controllers.ports.tabs;
 import com.exalttech.trex.ui.models.ConfigurationMode;
 import com.exalttech.trex.ui.models.PortModel;
 import com.exalttech.trex.util.Initialization;
+import javafx.beans.property.SimpleBooleanProperty;
 import javafx.beans.value.ChangeListener;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 
+import java.util.Arrays;
+
 public class PortLayerConfiguration extends BorderPane {
     @FXML
     private AnchorPane root;
-
     @FXML
     private ToggleGroup mode;
 
     @FXML
-    private TextField source;
-    
+    private TextField l2Source;
     @FXML
-    private TextField destination;
+    private TextField l3Source;
+
+    @FXML
+    private TextField l2Destination;
+
+    @FXML
+    private TextField l3Destination;
 
     @FXML
     private Label pingLabel;
-    
+
     @FXML
     private TextField pingDestination;
-    
+
     @FXML
     private Button pingCommandBtn;
+
+    @FXML
+    private Button saveBtn;
+
     @FXML
     private Label arpStatus;
+
     @FXML
     private Label arpLabel;
 
     @FXML
     RadioButton l2Mode;
+
     @FXML
     RadioButton l3Mode;
-    
     private PortModel model;
-    
-    private ChangeListener<ConfigurationMode> configurationModeChangeListener = (observable, prevMode, mode) -> updateControlsState(mode);
 
-    private void updateControlsState(ConfigurationMode type) {
-        source.setText(model.getLayerConfiguration().getSrc());
-        destination.setText(model.getLayerConfiguration().getDst());
+    private SimpleBooleanProperty configurationChanged = new SimpleBooleanProperty(false);
+
+    private ChangeListener<ConfigurationMode> configurationModeChangeListener = (observable, prevMode, mode) -> updateControlsState();
+
+    private void updateControlsState() {
+        Arrays.asList(l2Source, l2Destination, l3Source, l3Destination).forEach(textField -> {
+            textField.setVisible(false);
+            textField.setManaged(false);
+        });
         
-        if (ConfigurationMode.L2.equals(type)) {
+        if (ConfigurationMode.L2.equals(model.getLayerMode())) {
+            l2Source.setVisible(true);
+            l2Destination.setVisible(true);
+            l2Source.setManaged(true);
+            l2Destination.setManaged(true);
+            
             l2Mode.setSelected(true);
-            source.setDisable(true);
             arpStatus.setVisible(false);
             arpLabel.setVisible(false);
             pingLabel.setVisible(false);
             pingDestination.setVisible(false);
             pingCommandBtn.setVisible(false);
         } else {
-            arpStatus.textProperty().bind(model.getLayerConfiguration().stateProperty());
+            l3Source.setVisible(true);
+            l3Destination.setVisible(true);
+            l3Source.setManaged(true);
+            l3Destination.setManaged(true);
+            arpStatus.textProperty().bind(model.getL3LayerConfiguration().stateProperty());
             l3Mode.setSelected(true);
-            source.setDisable(false);
             arpLabel.setVisible(true);
             arpStatus.setVisible(true);
             pingLabel.setVisible(true);
@@ -70,27 +93,41 @@ public class PortLayerConfiguration extends BorderPane {
 
     public PortLayerConfiguration() {
         Initialization.initializeFXML(this, "/fxml/ports/PortLayerConfiguration.fxml");
+        
         l2Mode.setOnAction(event -> model.setLayerMode(ConfigurationMode.L2));
+        
         l3Mode.setOnAction(event -> model.setLayerMode(ConfigurationMode.L3));
+        
+        saveBtn.disableProperty().bind(configurationChanged.not());
     }
 
     public void bindModel(PortModel model) {
         unbindAll();
         this.model = model;
         
-        updateControlsState(this.model.getLayerConfiguration().getType());
+        l2Destination.textProperty().bindBidirectional(this.model.getL2LayerConfiguration().dstProperty());
+        l2Source.textProperty().bindBidirectional(this.model.getL2LayerConfiguration().srcProperty());
+        
+        l3Destination.textProperty().bindBidirectional(this.model.getL3LayerConfiguration().dstProperty());
+        l3Source.textProperty().bindBidirectional(this.model.getL3LayerConfiguration().srcProperty());
+        
+        updateControlsState();
         
         this.model.layerConfigurationTypeProperty().addListener(configurationModeChangeListener);
     }
 
-    
-    
+    public SimpleBooleanProperty configurationChangedProperty() {
+        return configurationChanged;
+    }
+
     private void unbindAll() {
         if(model == null) {
             return;
         }
-        source.textProperty().unbind();
-        destination.textProperty().unbind();
+        l2Destination.textProperty().unbind();
+        l2Source.textProperty().unbind();
+        l3Destination.textProperty().bindBidirectional(this.model.getL3LayerConfiguration().dstProperty());
+        l3Source.textProperty().unbind();
         arpStatus.textProperty().unbind();
         model.layerConfigurationTypeProperty().removeListener(configurationModeChangeListener);
     }
