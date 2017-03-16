3044f1def66927f1d5d7f3543f33e907d88c3721
Vyacheslav Ogai <vyacheslav.ogai@gmail.com>
2017-03-16 13:03:54 +0700

    Add binding for port attribute controls

--- src/main/java/com/exalttech/trex/ui/controllers/ports/tabs/PortAttributes.java
+++ src/main/java/com/exalttech/trex/ui/controllers/ports/tabs/PortAttributes.java
@@ -1,6 +1,7 @@
 package com.exalttech.trex.ui.controllers.ports.tabs;
 
 import com.exalttech.trex.ui.models.FlowControl;
+import com.exalttech.trex.ui.models.PortModel;
 import com.exalttech.trex.util.Initialization;
 import javafx.fxml.FXML;
 import javafx.scene.control.ChoiceBox;
@@ -11,57 +12,82 @@ import org.controlsfx.control.ToggleSwitch;
 public class PortAttributes extends BorderPane {
     
     @FXML
-    private PortAttributes attributesTab;
+    private Label driver;
 
     @FXML
-    private PortLayerConfiguration layerConfigTab;
+    private Label rxFilterMode;
 
     @FXML
-    private PortHardwareCounters hardwareCounters;
+    private ToggleSwitch multicast;
 
     @FXML
-    Label driver;
+    private ToggleSwitch promiscuousMode;
 
     @FXML
-    Label rxFilterMode;
+    private Label owner;
 
     @FXML
-    Label multicast;
+    private Label speed;
 
     @FXML
-    Label promiscuousMode;
+    private Label status;
 
     @FXML
-    Label owner;
+    private Label captureStatus;
 
     @FXML
-    Label speed;
+    private ToggleSwitch link;
 
     @FXML
-    Label status;
+    private ToggleSwitch led;
 
     @FXML
-    Label captureStatus;
+    private Label numaMode;
 
     @FXML
-    ToggleSwitch link;
+    private Label pciAddress;
 
     @FXML
-    ToggleSwitch led;
+    private Label gratArp;
 
     @FXML
-    ToggleSwitch numaMode;
-
-    @FXML
-    Label pciAddress;
-
-    @FXML
-    Label gratArp;
-
-    @FXML
-    ChoiceBox<FlowControl> flowControl;
+    private ChoiceBox<FlowControl> flowControl;
     
     public PortAttributes() {
         Initialization.initializeFXML(this, "/fxml/ports/PortAttributes.fxml");
+    }
+
+    public void bindModel(PortModel model) {
+        unbindPrevious();
+        
+        driver.textProperty().bind(model.portDriverProperty());
+        rxFilterMode.textProperty().bind(model.rxFilterModeProperty());
+        multicast.selectedProperty().bindBidirectional(model.multicastProperty());
+        promiscuousMode.selectedProperty().bindBidirectional(model.promiscuousModeProperty());
+        owner.textProperty().bind(model.ownerProperty());
+        speed.textProperty().bind(model.ownerProperty());
+        status.textProperty().bind(model.portStatusProperty());
+        captureStatus.textProperty().bind(model.capturingModeProperty());
+        link.selectedProperty().bindBidirectional(model.linkStatusProperty());
+        led.selectedProperty().bindBidirectional(model.ledStatusProperty());
+        numaMode.textProperty().bind(model.numaModeProperty());
+        pciAddress.textProperty().bind(model.numaModeProperty());
+        gratArp.textProperty().bind(model.gratARPProperty());
+    }
+
+    private void unbindPrevious() {
+        driver.textProperty().unbind();
+        rxFilterMode.textProperty().unbind();
+        multicast.selectedProperty().unbind();
+        promiscuousMode.selectedProperty().unbind();
+        owner.textProperty().unbind();
+        speed.textProperty().unbind();
+        status.textProperty().unbind();
+        captureStatus.textProperty().unbind();
+        link.selectedProperty().unbind();
+        led.selectedProperty().unbind();
+        numaMode.textProperty().unbind();
+        pciAddress.textProperty().unbind();
+        gratArp.textProperty().unbind();
     }
 }
