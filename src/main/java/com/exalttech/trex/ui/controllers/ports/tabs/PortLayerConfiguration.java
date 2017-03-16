1403b53be19725d105256e32c1a23ef201611c7a
Vyacheslav Ogai <vyacheslav.ogai@gmail.com>
2017-03-16 16:48:31 +0700

    Inject layer configuration controls to component.

--- src/main/java/com/exalttech/trex/ui/controllers/ports/tabs/PortLayerConfiguration.java
+++ src/main/java/com/exalttech/trex/ui/controllers/ports/tabs/PortLayerConfiguration.java
@@ -2,6 +2,9 @@ package com.exalttech.trex.ui.controllers.ports.tabs;
 
 import com.exalttech.trex.util.Initialization;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 
@@ -9,6 +12,21 @@ public class PortLayerConfiguration extends BorderPane {
     @FXML
     private AnchorPane root;
 
+    @FXML
+    private ToggleGroup mode;
+
+    @FXML
+    private TextField source;
+    
+    @FXML
+    private TextField destination;
+    
+    @FXML
+    private TextField pingDestination;
+    
+    @FXML
+    private Label arpStatus;
+    
     public PortLayerConfiguration() {
         Initialization.initializeFXML(this, "/fxml/ports/PortLayerConfiguration.fxml");
     }
