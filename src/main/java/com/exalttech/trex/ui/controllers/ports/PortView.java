a213a23a1dfc500a4fe8ccf419fea73ff1029398
Vyacheslav Ogai <vyacheslav.ogai@gmail.com>
2017-03-16 13:04:44 +0700

    Add loadModel method to PortView

--- src/main/java/com/exalttech/trex/ui/controllers/ports/PortView.java
+++ src/main/java/com/exalttech/trex/ui/controllers/ports/PortView.java
@@ -3,6 +3,7 @@ package com.exalttech.trex.ui.controllers.ports;
 import com.exalttech.trex.ui.controllers.ports.tabs.PortAttributes;
 import com.exalttech.trex.ui.controllers.ports.tabs.PortHardwareCounters;
 import com.exalttech.trex.ui.controllers.ports.tabs.PortLayerConfiguration;
+import com.exalttech.trex.ui.models.PortModel;
 import com.exalttech.trex.util.Initialization;
 import javafx.fxml.FXML;
 import javafx.scene.control.TabPane;
@@ -21,5 +22,9 @@ public class PortView extends TabPane {
     public PortView() {
         Initialization.initializeFXML(this, "/fxml/ports/Port.fxml");
         
+    }
+
+    public void loadModel(PortModel model) {
+        portAttributes.bindModel(model);
     }
 }
