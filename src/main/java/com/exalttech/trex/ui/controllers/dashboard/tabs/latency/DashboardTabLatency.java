b5d51699c0297abc45d3e37bd5bd109e2b9e43e1
Pavel Agapov <pavel.agapov@xored.com>
2017-03-20 18:44:10 +0700

    Change dashboard latency table orientation.

--- src/main/java/com/exalttech/trex/ui/controllers/dashboard/tabs/latency/DashboardTabLatency.java
+++ src/main/java/com/exalttech/trex/ui/controllers/dashboard/tabs/latency/DashboardTabLatency.java
@@ -1,19 +1,18 @@
 package com.exalttech.trex.ui.controllers.dashboard.tabs.latency;
 
-import com.exalttech.trex.ui.models.stats.flow.StatsFlowStream;
 import javafx.fxml.FXML;
-import javafx.scene.control.TableView;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
 
-import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.atomic.AtomicInteger;
 
+import com.exalttech.trex.ui.models.stats.flow.StatsFlowStream;
 import com.exalttech.trex.ui.models.stats.latency.StatsLatencyStream;
-import com.exalttech.trex.ui.models.stats.latency.StatsLatencyStreamErrCntrs;
-import com.exalttech.trex.ui.models.stats.latency.StatsLatencyStreamLatency;
-import com.exalttech.trex.ui.models.stats.latency.StatsLatencyTableRow;
+import com.exalttech.trex.ui.views.statistics.cells.CellType;
+import com.exalttech.trex.ui.views.statistics.cells.HeaderCell;
+import com.exalttech.trex.ui.views.statistics.cells.StatisticLabelCell;
 import com.exalttech.trex.ui.views.statistics.StatsLoader;
 import com.exalttech.trex.util.ArrayHistory;
 import com.exalttech.trex.util.Initialization;
@@ -21,44 +20,36 @@ import com.exalttech.trex.util.Initialization;
 
 public class DashboardTabLatency extends AnchorPane {
     @FXML
-    private TableView table;
+    private GridPane table;
 
     public DashboardTabLatency() {
         Initialization.initializeFXML(this, "/fxml/Dashboard/tabs/latency/DashboardTabLatency.fxml");
     }
 
     public void update(Set<Integer> visiblePorts, Set<String> visibleStreams) {
-        if (visibleStreams != null && visibleStreams.isEmpty()) {
-            table.getItems().clear();
-            return;
-        }
+        int firstColumnWidth = 120;
+        int secondHeaderWidth = 150;
 
-        StatsLatencyTableRow selectedRow = ((StatsLatencyTableRow) table.getSelectionModel().getSelectedItem());
-        String selectedStream = selectedRow != null ? selectedRow.getPgId() : null;
+        table.getChildren().clear();
+
+        table.add(new HeaderCell(firstColumnWidth, "PG ID"), 0, 0);
+        table.add(new StatisticLabelCell("Tx pkt", firstColumnWidth, false, CellType.DEFAULT_CELL, false), 0, 1);
+        table.add(new StatisticLabelCell("Rx pkt", firstColumnWidth, true, CellType.DEFAULT_CELL, false), 0, 2);
+        table.add(new StatisticLabelCell("Max Latency", firstColumnWidth, false, CellType.DEFAULT_CELL, false), 0, 3);
+        table.add(new StatisticLabelCell("Avg Latency", firstColumnWidth, true, CellType.DEFAULT_CELL, false), 0, 4);
+        table.add(new StatisticLabelCell("Jitter", firstColumnWidth, false, CellType.DEFAULT_CELL, false), 0, 5);
+        table.add(new StatisticLabelCell("Dropped", firstColumnWidth, true, CellType.DEFAULT_CELL, false), 0, 6);
+        table.add(new StatisticLabelCell("Dup", firstColumnWidth, false, CellType.DEFAULT_CELL, false), 0, 7);
+        table.add(new StatisticLabelCell("Out Of Order", firstColumnWidth, true, CellType.DEFAULT_CELL, false), 0, 8);
+        table.add(new StatisticLabelCell("Seq To High", firstColumnWidth, false, CellType.DEFAULT_CELL, false), 0, 9);
+        table.add(new StatisticLabelCell("Seq To Low", firstColumnWidth, true, CellType.DEFAULT_CELL, false), 0, 10);
 
         Map<String, StatsLatencyStream> latencyStatsByStreams = StatsLoader.getInstance().getLatencyStatsMap();
         Map<String, ArrayHistory<StatsFlowStream>> flowStatsMap = StatsLoader.getInstance().getFlowStatsHistoryMap();
 
-        Map<String, Integer> streamIndexes = buildStreamsIndexesMap();
-        Set<String> visitedStreams = new HashSet<>();
-        latencyStatsByStreams.forEach((String stream, StatsLatencyStream latencyStats) -> {
-            visitedStreams.add(stream);
-
+        AtomicInteger rowIndex = new AtomicInteger(1);
+        latencyStatsByStreams.forEach((String stream, StatsLatencyStream latencyStream) -> {
             if (visibleStreams != null && !visibleStreams.contains(stream)) {
-                return;
-            }
-
-            if (latencyStats == null) {
-                return;
-            }
-
-            StatsLatencyStreamLatency latency = latencyStats.getLatency();
-            if (latency == null) {
-                return;
-            }
-
-            StatsLatencyStreamErrCntrs errCntrs = latencyStats.getErrCntrs();
-            if (errCntrs == null) {
                 return;
             }
 
@@ -72,53 +63,19 @@ public class DashboardTabLatency extends AnchorPane {
                 return;
             }
 
-            StatsLatencyTableRow row = new StatsLatencyTableRow();
-            row.setPgId(stream);
-            row.setTxPkts(flowStats.calcTotalTxPkts(visiblePorts));
-            row.setRxPkts(flowStats.calcTotalRxPkts(visiblePorts));
-            row.setMaxLatency(latency.getTotalMax());
-            row.setAvgLatency(((int)(latency.getAverage()*100.0))/100.0);
-            row.setJitter(latency.getJitter());
-            row.setDup(errCntrs.getDup());
-            row.setDropped(errCntrs.getDropped());
-            row.setOutOfOrder(errCntrs.getOutOfOrder());
-            row.setSeqToHigh(errCntrs.getSeqTooHigh());
-            row.setSeqToLow(errCntrs.getSeqTooLow());
+            table.add(new HeaderCell(secondHeaderWidth, stream), rowIndex.get(), 0);
+            table.add(new StatisticLabelCell(String.valueOf(flowStats.calcTotalTxPkts(visiblePorts)), secondHeaderWidth, false, CellType.DEFAULT_CELL, true), rowIndex.get(), 1);
+            table.add(new StatisticLabelCell(String.valueOf(flowStats.calcTotalRxPkts(visiblePorts)), secondHeaderWidth, true, CellType.DEFAULT_CELL, true), rowIndex.get(), 2);
+            table.add(new StatisticLabelCell(String.valueOf(latencyStream.getLatency().getTotalMax()), secondHeaderWidth, false, CellType.DEFAULT_CELL, true), rowIndex.get(), 3);
+            table.add(new StatisticLabelCell(String.valueOf(latencyStream.getLatency().getAverage()), secondHeaderWidth, true, CellType.DEFAULT_CELL, true), rowIndex.get(), 4);
+            table.add(new StatisticLabelCell(String.valueOf(latencyStream.getLatency().getJitter()), secondHeaderWidth, false, CellType.DEFAULT_CELL, true), rowIndex.get(), 5);
+            table.add(new StatisticLabelCell(String.valueOf(latencyStream.getErrCntrs().getDropped()), secondHeaderWidth, true, CellType.DEFAULT_CELL, true), rowIndex.get(), 6);
+            table.add(new StatisticLabelCell(String.valueOf(latencyStream.getErrCntrs().getDup()), secondHeaderWidth, false, CellType.DEFAULT_CELL, true), rowIndex.get(), 7);
+            table.add(new StatisticLabelCell(String.valueOf(latencyStream.getErrCntrs().getOutOfOrder()), secondHeaderWidth, true, CellType.DEFAULT_CELL, true), rowIndex.get(), 8);
+            table.add(new StatisticLabelCell(String.valueOf(latencyStream.getErrCntrs().getSeqTooHigh()), secondHeaderWidth, false, CellType.DEFAULT_CELL, true), rowIndex.get(), 9);
+            table.add(new StatisticLabelCell(String.valueOf(latencyStream.getErrCntrs().getSeqTooLow()), secondHeaderWidth, true, CellType.DEFAULT_CELL, true), rowIndex.get(), 10);
 
-            Integer streamIndex = streamIndexes.get(stream);
-            if (streamIndex == null) {
-                table.getItems().add(row);
-            } else {
-                table.getItems().set(streamIndex, row);
-            }
+            rowIndex.addAndGet(1);
         });
-
-        table.getItems().removeIf((Object row) -> !visitedStreams.contains(((StatsLatencyTableRow) row).getPgId()));
-
-        selectStream(selectedStream);
-    }
-
-    private Map<String, Integer> buildStreamsIndexesMap() {
-        Map<String, Integer> streamIndexes = new HashMap<>();
-        int rowsCount = table.getItems().size();
-        for (int i = 0; i < rowsCount; ++i) {
-            StatsLatencyTableRow row = (StatsLatencyTableRow) table.getItems().get(i);
-            streamIndexes.put(row.getPgId(), i);
-        }
-        return streamIndexes;
-    }
-
-    private void selectStream(String stream) {
-        if (stream == null) {
-            return;
-        }
-        int rowsCount = table.getItems().size();
-        for (int i = 0; i < rowsCount; ++i) {
-            StatsLatencyTableRow row = (StatsLatencyTableRow) table.getItems().get(i);
-            if (row.getPgId().equals(stream)) {
-                table.getSelectionModel().select(i);
-                break;
-            }
-        }
     }
 }
