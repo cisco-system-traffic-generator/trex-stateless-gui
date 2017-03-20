75f60ddd36f5a14ef53808dfd298cb60ccbbafa9
Vyacheslav Ogai <vyacheslav.ogai@gmail.com>
2017-03-20 17:24:40 +0700

    Add Acquire/Release logic

--- src/main/java/com/exalttech/trex/ui/controllers/ports/tabs/PortAttributes.java
+++ src/main/java/com/exalttech/trex/ui/controllers/ports/tabs/PortAttributes.java
@@ -1,15 +1,28 @@
 package com.exalttech.trex.ui.controllers.ports.tabs;
 
+import com.exalttech.trex.application.TrexApp;
+import com.exalttech.trex.core.RPCMethods;
 import com.exalttech.trex.ui.models.FlowControl;
 import com.exalttech.trex.ui.models.PortModel;
+import com.exalttech.trex.ui.views.logs.LogType;
+import com.exalttech.trex.ui.views.logs.LogsController;
 import com.exalttech.trex.util.Initialization;
+import com.google.common.base.Strings;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
+import org.apache.log4j.Logger;
 import org.controlsfx.control.ToggleSwitch;
 
 public class PortAttributes extends BorderPane {
+
+    private static final Logger logger = Logger.getLogger(PortAttributes.class);
+
+    private final RPCMethods trexClient = TrexApp.injector.getInstance(RPCMethods.class);
+    
+    private PortModel port;
     
     @FXML
     private Label driver;
@@ -53,12 +66,76 @@ public class PortAttributes extends BorderPane {
     @FXML
     private ChoiceBox<FlowControl> flowControl;
     
+    @FXML
+    private Button acquireReleaseBtn;
+    @FXML
+    private Button forceAcquireBtn;
+    
+    
+    
     public PortAttributes() {
         Initialization.initializeFXML(this, "/fxml/ports/PortAttributes.fxml");
+
+        acquireReleaseBtn.setOnAction(event -> {
+            try {
+                acquireReleaseBtn.setDisable(true);
+                forceAcquireBtn.setDisable(true);
+                if (acquireReleaseBtn.getText().equalsIgnoreCase("Acquire")) {
+                    trexClient.acquireServerPort(port.getIndex(), false);
+                    acquireReleaseBtn.setText("Release");
+                } else {
+                    trexClient.releasePort(port.getIndex(), true);
+                    acquireReleaseBtn.setText("Acquire");
+                    forceAcquireBtn.setDisable(false);
+                }
+                acquireReleaseBtn.setDisable(false);
+                
+            } catch (Exception e) {
+                acquireReleaseBtn.setDisable(false);
+                forceAcquireBtn.setDisable(false);
+                
+                String message = "Unable acquire port " + port.getIndex();
+                logger.error(message, e);
+                LogsController.getInstance().appendText(LogType.ERROR, message);
+                
+            }
+        });
+        
+        forceAcquireBtn.setOnAction(event -> {
+            try {
+                acquireReleaseBtn.setDisable(true);
+                forceAcquireBtn.setDisable(true);
+                trexClient.acquireServerPort(port.getIndex(), true);
+                acquireReleaseBtn.setDisable(false);
+                acquireReleaseBtn.setText("Release");
+            } catch (Exception e) {
+                acquireReleaseBtn.setDisable(false);
+                forceAcquireBtn.setDisable(false);
+
+                String message = "Unable acquire port " + port.getIndex();
+                logger.error(message, e);
+                LogsController.getInstance().appendText(LogType.ERROR, message);
+
+            }
+        });
     }
 
     public void bindModel(PortModel model) {
+
         unbindPrevious();
+        
+        this.port = model;
+        
+        acquireReleaseBtn.setDisable(false);
+        forceAcquireBtn.setDisable(false);
+        
+        if (this.port.isOwnedProperty().get()) {
+            acquireReleaseBtn.setText("Release");
+            forceAcquireBtn.setDisable(true);
+        } else {
+            acquireReleaseBtn.setText("Acquire");
+            acquireReleaseBtn.setDisable(!Strings.isNullOrEmpty(this.port.getOwner()));
+        }
         
         driver.textProperty().bind(model.portDriverProperty());
         rxFilterMode.textProperty().bind(model.rxFilterModeProperty());
@@ -89,5 +166,6 @@ public class PortAttributes extends BorderPane {
         numaMode.textProperty().unbind();
         pciAddress.textProperty().unbind();
         gratArp.textProperty().unbind();
+        port = null;
     }
 }
