74a37451a57fd520c2b3bd1c49f35ff7e5933edd
Pavel Agapov <pavel.agapov@xored.com>
2017-04-18 12:00:21 +0700

    Add basic connection tests.

---
+++ src/ui-test/java/com/exalttech/trex/TestBase.java
@@ -0,0 +1,31 @@
+package com.exalttech.trex;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import org.testfx.framework.junit.ApplicationTest;
+
+import com.exalttech.trex.application.TrexApp;
+
+
+public class TestBase extends ApplicationTest {
+    @Override
+    public void start(Stage stage) throws Exception {
+        TrexApp.setPrimaryStage(stage);
+        final AnchorPane page = FXMLLoader.load(getClass().getResource("/fxml/MainView.fxml"));
+        final Scene scene = new Scene(page);
+        scene.getStylesheets().add(TrexApp.class.getResource("/styles/mainStyle.css").toExternalForm());
+        stage.setScene(scene);
+        stage.setTitle("TRex");
+        stage.setResizable(true);
+        stage.setMinWidth(1100);
+        stage.setMinHeight(670);
+        stage.show();
+    }
+
+    public String getTRexServerIP() {
+        return "192.168.50.150";
+    }
+}
---
+++ src/ui-test/java/com/exalttech/trex/TestConnection.java
@@ -0,0 +1,43 @@
+package com.exalttech.trex;
+
+import javafx.scene.input.KeyCode;
+
+import static org.testfx.api.FxAssert.verifyThat;
+
+import org.junit.Test;
+
+import org.testfx.matcher.base.NodeMatchers;
+
+
+public class TestConnection extends TestBase {
+    @Test
+    public void testConnectDisconnectFromToolbar(){
+        clickOn(".connectIcon");
+        clickOn("#connection-dialog-ip");
+        push(KeyCode.CONTROL, KeyCode.A);
+        push(KeyCode.DELETE);
+        write(getTRexServerIP());
+        clickOn("#connection-dialog-connect");
+        sleep(5000);
+        verifyThat("#connection-dialog", NodeMatchers.isNull());
+        clickOn(".disconnectIcon");
+        sleep(1000);
+        verifyThat(".disconnectIcon", NodeMatchers.isNull());
+    }
+
+    @Test
+    public void testConnectDisconnectFromMenu(){
+        clickOn("#main-menu");
+        clickOn("#main-menu-connect");
+        clickOn("#connection-dialog-ip");
+        push(KeyCode.CONTROL, KeyCode.A);
+        push(KeyCode.DELETE);
+        write(getTRexServerIP());
+        clickOn("#connection-dialog-connect");
+        sleep(5000);
+        verifyThat("#connection-dialog", NodeMatchers.isNull());
+        clickOn(".disconnectIcon");
+        sleep(1000);
+        verifyThat(".disconnectIcon", NodeMatchers.isNull());
+    }
+}
