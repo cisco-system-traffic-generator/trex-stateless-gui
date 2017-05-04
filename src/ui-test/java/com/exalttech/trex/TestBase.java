ab5bd9f750a8064911c97d8f36d9dd708583486f
Vyacheslav Ogai <vyacheslav.ogai@gmail.com>
2017-05-04 23:24:48 +0700

    Update TestBase: remove duplicate code, add tryCall() isVisible() methods.

--- src/ui-test/java/com/exalttech/trex/TestBase.java
+++ src/ui-test/java/com/exalttech/trex/TestBase.java
@@ -1,32 +1,33 @@
 package com.exalttech.trex;
 
+import com.exalttech.trex.application.TrexApp;
+import com.exalttech.trex.core.ConnectionManager;
 import javafx.scene.Node;
+import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
 import javafx.stage.Stage;
-
 import org.junit.Assert;
-
 import org.testfx.framework.junit.ApplicationTest;
 
+import java.util.Optional;
 import java.util.concurrent.*;
-
-import com.exalttech.trex.application.TrexApp;
-import com.exalttech.trex.core.ConnectionManager;
 
 
 public class TestBase extends ApplicationTest {
+    private ExecutorService executor = Executors.newSingleThreadExecutor();
+
     enum MenuType {
         MENU,
         SHORTCUT,
         TOOLBAR
     }
 
-    static private final long TIMEOUT_S = 10;
+    static private final long TIMEOUT_S = 60;
     static private final long POLLING_INTERVAL_MS = 100;
-    static private final long RENDER_DELAY_MS = 1000;
+    static private final long RENDER_DELAY_MS = 2500;
 
     private TrexApp app;
 
@@ -42,7 +43,7 @@ public class TestBase extends ApplicationTest {
         app.stop();
     }
 
-    private void setText(final String query, final String text) {
+    protected void setText(final String query, final String text) {
         final Node node = lookup(query).query();
         if (node instanceof TextField) {
             ((TextField) node).setText(text);
@@ -63,41 +64,28 @@ public class TestBase extends ApplicationTest {
         return null;
     }
 
-    String getTRexServerIP() {
-        return "192.168.50.154";
+    protected String getTRexServerIP() {
+        return "trex-host";
     }
 
-    private String getTRexSyncPort() {
+    protected String getTRexSyncPort() {
         return "4501";
     }
 
-    private String getTRexAsyncPort() {
+    protected String getTRexAsyncPort() {
         return "4500";
     }
 
-    private String getTRexScapyPort() {
-        return "4500";
+    protected String getTRexScapyPort() {
+        return "4507";
     }
 
-    String getTRexDefaultUser() {
+    protected String getTRexDefaultUser() {
         return "Test";
     }
 
     boolean connect(final MenuType menuType, final String user) {
-        switch (menuType) {
-            case MENU:
-                clickOn("#main-menu");
-                clickOn("#main-menu-connect");
-                break;
-            case SHORTCUT:
-                push(KeyCode.CONTROL, KeyCode.C);
-                break;
-            case TOOLBAR:
-                clickOn("#main-toolbar-connect");
-                break;
-            default:
-                return false;
-        }
+        connectOrDisconnectAction(menuType);
         sleep(RENDER_DELAY_MS);
 
         setText("#connection-dialog-ip", getTRexServerIP());
@@ -109,9 +97,8 @@ public class TestBase extends ApplicationTest {
 
         clickOn("#connection-dialog-connect");
 
-        final ExecutorService executor = Executors.newSingleThreadExecutor();
         final Future<Boolean> future = executor.submit(() -> {
-            while (!ConnectionManager.getInstance().isConnected()) {
+            while (!isConnected()) {
                 sleep(POLLING_INTERVAL_MS);
             }
             return true;
@@ -122,7 +109,6 @@ public class TestBase extends ApplicationTest {
         } catch (Exception e) {
             future.cancel(true);
         }
-        executor.shutdownNow();
 
         if (connected) {
             sleep(RENDER_DELAY_MS);
@@ -131,28 +117,44 @@ public class TestBase extends ApplicationTest {
         return connected;
     }
 
+    private boolean isConnected() {
+        return !isDisconnected();
+    }
+
+    protected Boolean isDisconnected() {
+        Label connection = lookup("#main-server-status").query();
+        return "disconnected".equalsIgnoreCase(connection.getText());
+    }
+
     boolean connect(final MenuType menuType) {
         return connect(menuType, getTRexDefaultUser());
     }
 
-    boolean disconnect(final MenuType menuType) {
-        switch (menuType) {
-            case MENU:
-                clickOn("#main-menu");
-                clickOn("#main-menu-connect");
-                break;
-            case SHORTCUT:
-                push(KeyCode.CONTROL, KeyCode.C);
-                break;
-            case TOOLBAR:
-                clickOn("#main-toolbar-connect");
-                break;
-            default:
-                return false;
+    protected Boolean tryCall(Runnable action, Callable<Boolean> resultValidator) {
+        final Future<Boolean> future = executor.submit(() -> {
+            try {
+                action.run();
+            } catch (Exception e) {
+                return null;
+            }
+            while (!resultValidator.call()) {
+                sleep(POLLING_INTERVAL_MS);
+            }
+            return true;
+        });
+
+        try {
+            return future.get(TIMEOUT_S, TimeUnit.SECONDS);
+        } catch (Exception e) {
+            future.cancel(true);
+            return false;
         }
+    }
+    
+    boolean disconnect(final MenuType menuType) {
+        connectOrDisconnectAction(menuType);
         sleep(RENDER_DELAY_MS);
 
-        final ExecutorService executor = Executors.newSingleThreadExecutor();
         final Future<Boolean> future = executor.submit(() -> {
             while (ConnectionManager.getInstance().isConnected()) {
                 sleep(POLLING_INTERVAL_MS);
@@ -165,7 +167,6 @@ public class TestBase extends ApplicationTest {
         } catch (Exception e) {
             future.cancel(true);
         }
-        executor.shutdownNow();
 
         if (disconnected) {
             sleep(RENDER_DELAY_MS);
@@ -174,10 +175,40 @@ public class TestBase extends ApplicationTest {
         return disconnected;
     }
 
+    private void connectOrDisconnectAction(MenuType menuType) {
+        switch (menuType) {
+            case MENU:
+                clickOn("#main-menu");
+                clickOn("#main-menu-connect");
+                break;
+            case SHORTCUT:
+                push(KeyCode.CONTROL, KeyCode.C);
+                break;
+            case TOOLBAR:
+                clickOn("#main-toolbar-connect");
+                break;
+        }
+    }
+
     void resetAllPorts() {
-        clickOn("Port 0");
-        clickOn("Reset");
-        clickOn("Port 1");
-        clickOn("Reset");
+        resetPort("Port 0");
+        resetPort("Port 1");
+    }
+    
+    private void resetPort(String port) {
+        clickOn(port);
+        tryCall(
+          () -> clickOn("#resetBtn"),
+          () -> {
+              Button resetBtn = lookup("#resetBtn").query();
+              return resetBtn.getText().equalsIgnoreCase("Reset");
+          }
+        );
+        
+    }
+
+    public boolean isVisible(String query) {
+        Optional<Node> result = lookup(query).tryQuery();
+        return result.isPresent() && result.get().isVisible();
     }
 }
