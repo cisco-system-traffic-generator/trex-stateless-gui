e78c9f2d2a54bead3ec07e5cb7fc40c4c9856d07
Vyacheslav Ogai <vyacheslav.ogai@gmail.com>
2017-06-02 16:15:06 +0700

    Add Packet Capture dashboard

--- src/main/java/com/exalttech/trex/ui/controllers/MainViewController.java
+++ src/main/java/com/exalttech/trex/ui/controllers/MainViewController.java
@@ -904,6 +904,29 @@ public class MainViewController implements Initializable, EventHandler<KeyEvent>
     }
 
     /**
+     * Handle stats menu item clicked
+     *
+     * @param event
+     */
+    @FXML
+    public void handleCaptureItemClicked(ActionEvent event) {
+        try {
+            DialogWindow statsWindow = new DialogWindow(
+                    "pkt_capture/Layout.fxml",
+                    "Packet Capture",
+                    50,
+                    10,
+                    true,
+                    TrexApp.getPrimaryStage()
+            );
+            statsWindow.setMinSize(1210, 740);
+            statsWindow.show(false);
+        } catch (IOException ex) {
+            LOG.error("Error opening dashboard view", ex);
+        }
+    }
+
+    /**
      * Open statistic dashboard view
      */
     private void openStateDialog() {
