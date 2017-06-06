d9588090600397ea16b57d58d8ceb8eea78f6af5
Vyacheslav Ogai <vyacheslav.ogai@gmail.com>
2017-06-06 09:22:16 +0700

    Add Layout for Monitor tab.

--- src/main/java/com/cisco/trex/stl/gui/controllers/Monitor.java
+++ src/main/java/com/cisco/trex/stl/gui/controllers/Monitor.java
@@ -1,7 +1,10 @@
 package com.cisco.trex.stl.gui.controllers;
 
+import com.exalttech.trex.util.Initialization;
 import javafx.scene.layout.AnchorPane;
 
 public class Monitor extends AnchorPane {
-    
+    public Monitor() {
+        Initialization.initializeFXML(this, "/fxml/pkt_capture/Monitor.fxml");
+    }
 }
