ff3152b70a3fa770d00b54a87f6b20b70b6c70f5
Vyacheslav Ogai <vyacheslav.ogai@gmail.com>
2017-06-06 09:24:47 +0700

    Add PortFilter component

---
+++ src/main/java/com/cisco/trex/stl/gui/controllers/PortFilter.java
@@ -0,0 +1,19 @@
+package com.cisco.trex.stl.gui.controllers;
+
+import com.exalttech.trex.util.Initialization;
+import javafx.fxml.FXML;
+import javafx.scene.layout.HBox;
+import org.controlsfx.control.CheckComboBox;
+
+public class PortFilter extends HBox {
+    
+    @FXML
+    private CheckComboBox rxFilter;
+    
+    @FXML
+    private CheckComboBox txFilter;
+
+    public PortFilter() {
+        Initialization.initializeFXML(this, "/fxml/pkt_capture/PortFilter.fxml");
+    }
+}
