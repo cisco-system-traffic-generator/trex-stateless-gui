05111fd5b238e76b5e314e32187ead8b035f84cf
Pavel Agapov <pavel.agapov@xored.com>
2017-06-09 16:53:00 +0700

    Update dashboard layout

--- src/main/java/com/cisco/trex/stl/gui/controllers/dashboard/DashboardController.java
+++ src/main/java/com/cisco/trex/stl/gui/controllers/dashboard/DashboardController.java
@@ -5,10 +5,7 @@ import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TabPane;
-import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
-import javafx.stage.WindowEvent;
-import javafx.util.Duration;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -16,16 +13,11 @@ import java.util.ResourceBundle;
 import com.cisco.trex.stl.gui.controllers.dashboard.charts.ChartsController;
 import com.cisco.trex.stl.gui.controllers.dashboard.latency.LatencyController;
 import com.cisco.trex.stl.gui.controllers.dashboard.ports.PortsController;
-import com.cisco.trex.stl.gui.controllers.dashboard.selectors.ports.PortsSelectorController;
-import com.cisco.trex.stl.gui.controllers.dashboard.selectors.streams.StreamsSelectorController;
 import com.cisco.trex.stl.gui.controllers.dashboard.streams.StreamsController;
 import com.cisco.trex.stl.gui.storages.StatsStorage;
 
 import com.exalttech.trex.ui.dialog.DialogView;
-import com.exalttech.trex.ui.views.services.RefreshingService;
 import com.exalttech.trex.ui.views.statistics.StatsLoader;
-import com.exalttech.trex.util.Constants;
-import com.exalttech.trex.util.Initialization;
 
 
 public class DashboardController extends DialogView implements Initializable {
@@ -34,12 +26,6 @@ public class DashboardController extends DialogView implements Initializable {
     private static final String LATENCY_TAB_LABEL = "Latency";
     private static final String CHARTS_TAB_LABEL = "Charts";
 
-    @FXML
-    private BorderPane root;
-    @FXML
-    private PortsSelectorController portsSelector;
-    @FXML
-    private StreamsSelectorController streamsSelector;
     @FXML
     private TabPane tabPane;
     @FXML
@@ -51,16 +37,9 @@ public class DashboardController extends DialogView implements Initializable {
     @FXML
     private ChartsController charts;
 
-    private RefreshingService refreshingService;
-
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        refreshingService = new RefreshingService();
-        refreshingService.setPeriod(Duration.seconds(Constants.REFRESH_ONE_INTERVAL_SECONDS));
-        refreshingService.setOnSucceeded(this::handleUpdate);
-        refreshingService.start();
-
-        Initialization.initializeCloseEvent(root, this::onWindowCloseRequest);
+        // Nothing to do
     }
 
     @Override
@@ -72,8 +51,9 @@ public class DashboardController extends DialogView implements Initializable {
     public void handleTabChanged(final Event event) {
         switch (tabPane.getSelectionModel().getSelectedItem().getText()) {
             case PORTS_TAB_LABEL:
-                portsSelector.setVisible(true);
-                streamsSelector.setVisible(false);
+                if (ports != null) {
+                    ports.setActive(true);
+                }
                 if (streams != null) {
                     streams.setActive(false);
                 }
@@ -85,56 +65,29 @@ public class DashboardController extends DialogView implements Initializable {
                 }
                 break;
             case STREAMS_TAB_LABEL:
-                portsSelector.setVisible(false);
-                streamsSelector.setVisible(true);
+                ports.setActive(false);
                 streams.setActive(true);
                 latency.setActive(false);
                 charts.setActive(false);
                 break;
             case LATENCY_TAB_LABEL:
-                portsSelector.setVisible(false);
-                streamsSelector.setVisible(true);
+                ports.setActive(false);
                 streams.setActive(false);
                 latency.setActive(true);
                 charts.setActive(false);
                 break;
             case CHARTS_TAB_LABEL:
-                portsSelector.setVisible(false);
-                streamsSelector.setVisible(true);
+                ports.setActive(false);
                 streams.setActive(false);
                 latency.setActive(false);
                 charts.setActive(true);
                 break;
         }
-
-        final boolean isPortsTab = tabPane.getSelectionModel().getSelectedItem().getText().equals(PORTS_TAB_LABEL);
-        portsSelector.setVisible(isPortsTab);
-        streamsSelector.setVisible(!isPortsTab);
-    }
-
-    @FXML
-    public void handleFiltersChanged(final Event event) {
-        handleUpdate(event);
     }
 
     @FXML
     public void handleClearCacheButtonClicked(final ActionEvent event) {
         StatsLoader.getInstance().reset();
         StatsStorage.getInstance().getPGIDStatsStorage().reset();
-        handleUpdate(event);
-    }
-
-    private void handleUpdate(final Event event) {
-        final String selectedTab = tabPane.getSelectionModel().getSelectedItem().getText();
-        if (selectedTab.equals(PORTS_TAB_LABEL)) {
-            ports.update(portsSelector.getSelectedPortIndexes());
-        }
-    }
-
-    private void onWindowCloseRequest(WindowEvent window) {
-        if (refreshingService.isRunning()) {
-            refreshingService.cancel();
-        }
-        ports.reset();
     }
 }
