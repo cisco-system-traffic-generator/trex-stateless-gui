1f123c97a12b0e0b9f6f332bb0adda8d123edb31
Vyacheslav Ogai <vyacheslav.ogai@gmail.com>
2017-06-17 15:58:31 +0700

    Enable latency charts only if latency is enabled at least in one stream

--- src/main/java/com/cisco/trex/stl/gui/controllers/dashboard/charts/ChartContainerController.java
+++ src/main/java/com/cisco/trex/stl/gui/controllers/dashboard/charts/ChartContainerController.java
@@ -1,5 +1,9 @@
 package com.cisco.trex.stl.gui.controllers.dashboard.charts;
 
+import com.cisco.trex.stl.gui.util.RunningConfiguration;
+import com.exalttech.trex.application.TrexApp;
+import com.exalttech.trex.util.Initialization;
+import javafx.beans.property.BooleanProperty;
 import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
@@ -12,8 +16,6 @@ import javafx.scene.control.MenuItem;
 import javafx.scene.control.SeparatorMenuItem;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
-
-import com.exalttech.trex.util.Initialization;
 
 
 public class ChartContainerController extends AnchorPane {
@@ -29,6 +31,8 @@ public class ChartContainerController extends AnchorPane {
     private FlowChartController chart;
     private StringProperty chartType;
     private IntegerProperty interval;
+
+    private final RunningConfiguration runningConfiguration = TrexApp.injector.getInstance(RunningConfiguration.class);
 
     public ChartContainerController(String selectedType, IntegerProperty interval) {
         Initialization.initializeFXML(this, "/fxml/dashboard/charts/ChartContainer.fxml");
@@ -47,11 +51,11 @@ public class ChartContainerController extends AnchorPane {
                 createContextMenuItem(ChartsFactory.ChartTypes.TX_BPS_L2),
                 createContextMenuItem(ChartsFactory.ChartTypes.RX_BPS_L2),
                 new SeparatorMenuItem(),
-                createContextMenuItem(ChartsFactory.ChartTypes.MAX_LATENCY),
-                createContextMenuItem(ChartsFactory.ChartTypes.AVG_LATENCY),
-                createContextMenuItem(ChartsFactory.ChartTypes.JITTER_LATENCY),
-                createContextMenuItem(ChartsFactory.ChartTypes.TEMPORARY_MAX_LATENCY),
-                createContextMenuItem(ChartsFactory.ChartTypes.LATENCY_HISTOGRAM)
+                createContextMenuItem(ChartsFactory.ChartTypes.MAX_LATENCY, runningConfiguration.latencyEnabledProperty()),
+                createContextMenuItem(ChartsFactory.ChartTypes.AVG_LATENCY, runningConfiguration.latencyEnabledProperty()),
+                createContextMenuItem(ChartsFactory.ChartTypes.JITTER_LATENCY, runningConfiguration.latencyEnabledProperty()),
+                createContextMenuItem(ChartsFactory.ChartTypes.TEMPORARY_MAX_LATENCY, runningConfiguration.latencyEnabledProperty()),
+                createContextMenuItem(ChartsFactory.ChartTypes.LATENCY_HISTOGRAM, runningConfiguration.latencyEnabledProperty())
         );
     }
 
@@ -62,6 +66,11 @@ public class ChartContainerController extends AnchorPane {
     private MenuItem createContextMenuItem(String chartType) {
         MenuItem item = new MenuItem(chartType);
         item.setOnAction(this::handleContextMenuAction);
+        return item;
+    }
+    private MenuItem createContextMenuItem(String chartType, BooleanProperty enabledProperty) {
+        MenuItem item = createContextMenuItem(chartType);
+        item.disableProperty().bind(enabledProperty.not());
         return item;
     }
 
