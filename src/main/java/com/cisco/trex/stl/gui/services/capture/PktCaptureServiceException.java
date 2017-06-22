488e055958e9694d8afc5f92e15ccc9ac68f6e26
Vyacheslav Ogai <vyacheslav.ogai@gmail.com>
2017-06-22 23:21:59 +0700

    Add PktCaptureServiceException

---
+++ src/main/java/com/cisco/trex/stl/gui/services/capture/PktCaptureServiceException.java
@@ -0,0 +1,7 @@
+package com.cisco.trex.stl.gui.services.capture;
+
+public class PktCaptureServiceException extends Exception {
+    public PktCaptureServiceException(String message) {
+        super(message);
+    }
+}
