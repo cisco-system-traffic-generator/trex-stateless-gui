6afa8849241e1cf3e623482e2fb36142bc3f1b44
Vyacheslav Ogai <vyacheslav.ogai@gmail.com>
2017-06-23 10:03:48 +0700

    Integrate PktCaptureService into MonitorController

--- src/main/java/com/cisco/trex/stl/gui/controllers/capture/MonitorController.java
+++ src/main/java/com/cisco/trex/stl/gui/controllers/capture/MonitorController.java
@@ -1,36 +1,62 @@
 package com.cisco.trex.stl.gui.controllers.capture;
 
-import com.cisco.trex.stl.gui.models.CapturedPkt;
+import com.cisco.trex.stateless.model.capture.CapturedPkt;
+import com.cisco.trex.stl.gui.models.CapturedPktModel;
+import com.cisco.trex.stl.gui.services.capture.PktCaptureService;
+import com.cisco.trex.stl.gui.services.capture.PktCaptureServiceException;
 import com.exalttech.trex.util.Initialization;
+import javafx.concurrent.WorkerStateEvent;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.layout.BorderPane;
 
 public class MonitorController extends BorderPane {
-    
-    @FXML
-    private TableView<CapturedPkt> capturedPkts;
 
     @FXML
-    private TableColumn<CapturedPkt, String> number;
-    @FXML
-    private TableColumn<CapturedPkt, String> port;
-    @FXML
-    private TableColumn<CapturedPkt, String> mode;
-    @FXML
-    private TableColumn<CapturedPkt, String> time;
-    @FXML
-    private TableColumn<CapturedPkt, String> ipDst;
-    @FXML
-    private TableColumn<CapturedPkt, String> ipSrc;
-    @FXML
-    private TableColumn<CapturedPkt, String> type;
-    @FXML
-    private TableColumn<CapturedPkt, String> length;
-    @FXML
-    private TableColumn<CapturedPkt, String> info;
+    private Button startBtn;
     
+    @FXML
+    private Button stopBtn;
+    @FXML
+    private Button clearBtn;
+    
+    @FXML
+    private PortFilterController portFilter;
+    
+    @FXML
+    private TableView<CapturedPktModel> capturedPkts;
+
+    @FXML
+    private TableColumn<CapturedPktModel, String> number;
+    
+    @FXML
+    private TableColumn<CapturedPktModel, String> port;
+    
+    @FXML
+    private TableColumn<CapturedPktModel, String> mode;
+    
+    @FXML
+    private TableColumn<CapturedPktModel, String> time;
+    
+    @FXML
+    private TableColumn<CapturedPktModel, String> ipDst;
+    
+    @FXML
+    private TableColumn<CapturedPktModel, String> ipSrc;
+    
+    @FXML
+    private TableColumn<CapturedPktModel, String> type;
+    
+    @FXML
+    private TableColumn<CapturedPktModel, String> length;
+    
+    @FXML
+    private TableColumn<CapturedPktModel, String> info;
+    
+    private PktCaptureService pktCaptureService = new PktCaptureService();
     
     public MonitorController() {
         Initialization.initializeFXML(this, "/fxml/pkt_capture/Monitor.fxml");
@@ -45,29 +71,40 @@ public class MonitorController extends BorderPane {
         length.setCellValueFactory(cellData -> cellData.getValue().lengthProperty().asString());
         info.setCellValueFactory(cellData -> cellData.getValue().infoProperty());
         
-        capturedPkts.getItems().addAll(
-            new CapturedPkt(1, 1, "RX",  0.000001, "1.1.1.1", "1.1.1.22", "UDP", 64, "Ethernet/IPV4/UDP"),
-            new CapturedPkt(2, 1, "RX",  0.000002, "1.1.1.1", "1.1.1.23", "TCP", 64, "Ethernet/IPV4/TCP"),
-            new CapturedPkt(3, 1, "RX",  0.000003, "1.1.1.1", "1.1.1.24", "UDP", 64, "Ethernet/IPV4/UDP"),
-            new CapturedPkt(4, 1, "RX",  0.000004, "1.1.1.1", "1.1.1.25", "TCP", 64, "Ethernet/IPV4/TCP"),
-            new CapturedPkt(5, 1, "RX",  0.000005, "1.1.1.1", "1.1.1.26", "UDP", 64, "Ethernet/IPV4/UDP"),
-            new CapturedPkt(6, 1, "RX",  0.000006, "1.1.1.1", "1.1.1.27", "TCP", 64, "Ethernet/IPV4/TCP"),
-            new CapturedPkt(7, 1, "RX",  0.000007, "1.1.1.1", "1.1.1.28", "TCP", 64, "Ethernet/IPV4/TCP"),
-            new CapturedPkt(8, 1, "RX",  0.000008, "1.1.1.1", "1.1.1.29", "TCP", 64, "Ethernet/IPV4/TCP"),
-            new CapturedPkt(9, 1, "RX",  0.000009, "1.1.1.1", "1.1.1.10", "TCP", 64, "Ethernet/IPV4/TCP"),
-            new CapturedPkt(10, 1, "RX",  0.000011, "1.1.1.1", "1.1.1.11", "TCP", 64, "Ethernet/IPV4/TCP"),
-            new CapturedPkt(12, 1, "RX",  0.000021, "1.1.1.1", "1.1.1.12", "TCP", 64, "Ethernet/IPV4/TCP"),
-            new CapturedPkt(13, 1, "RX",  0.000031, "1.1.1.1", "1.1.1.13", "TCP", 64, "Ethernet/IPV4/TCP"),
-            new CapturedPkt(14, 1, "RX",  0.000041, "1.1.1.1", "1.1.1.14", "TCP", 64, "Ethernet/IPV4/TCP"),
-            new CapturedPkt(15, 1, "RX",  0.000051, "1.1.1.1", "1.1.1.15", "TCP", 64, "Ethernet/IPV4/TCP"),
-            new CapturedPkt(16, 1, "RX",  0.000061, "1.1.1.1", "1.1.1.16", "TCP", 64, "Ethernet/IPV4/TCP"),
-            new CapturedPkt(17, 1, "RX",  0.000071, "1.1.1.1", "1.1.1.17", "TCP", 64, "Ethernet/IPV4/TCP"),
-            new CapturedPkt(18, 1, "RX",  0.000081, "1.1.1.1", "1.1.1.18", "TCP", 64, "Ethernet/IPV4/TCP"),
-            new CapturedPkt(19, 1, "RX",  0.000081, "1.1.1.1", "1.1.1.19", "TCP", 64, "Ethernet/IPV4/TCP"),
-            new CapturedPkt(20, 1, "RX",  0.000091, "1.1.1.1", "1.1.1.20", "TCP", 64, "Ethernet/IPV4/TCP"),
-            new CapturedPkt(21, 1, "RX",  0.000021, "1.1.1.1", "1.1.1.21", "TCP", 64, "Ethernet/IPV4/TCP"),
-            new CapturedPkt(22, 1, "RX",  0.000031, "1.1.1.1", "1.1.1.22", "TCP", 64, "Ethernet/IPV4/TCP"),
-            new CapturedPkt(23, 1, "RX",  0.000041, "1.1.1.1", "2.2.2.25", "UDP", 64, "Ethernet/IPV4/UDP")
-        );
+        pktCaptureService.setOnSucceeded(this::handleOnPktsReceived);
+        
+        startBtn.setOnAction(this::handleStartMonitorAction);
+        stopBtn.setOnAction(this::handleStopMonitorAction);
+        clearBtn.setOnAction(this::handleClearMonitorAction);
+    }
+
+    private void handleOnPktsReceived(WorkerStateEvent workerStateEvent) {
+        synchronized (capturedPkts) {
+            pktCaptureService.getValue().getPkts().stream().map(this::toModel).forEach(pktModel -> capturedPkts.getItems().add(pktModel));
+        }
+    }
+
+    public void handleStartMonitorAction(ActionEvent event) {
+        try {
+            pktCaptureService.reset();
+            pktCaptureService.startMonitor(portFilter.getRxPorts(), portFilter.getTxPorts());
+        } catch (PktCaptureServiceException e) {
+            // TODO: logger
+        }
+    }
+    
+    public void handleStopMonitorAction(ActionEvent event) {
+        pktCaptureService.stopMonitor();
+        pktCaptureService.cancel();
+    }
+    
+    public void handleClearMonitorAction(ActionEvent event) {
+        synchronized (capturedPkts) {
+            capturedPkts.getItems().clear();
+        }
+    }
+    
+    private CapturedPktModel toModel(CapturedPkt pkt) {
+        return new CapturedPktModel(pkt.getIndex(), pkt.getPort(), pkt.getOrigin(),  0.000001, "0.0.0.0", "0.0.0.0", "?", 64, "?");
     }
 }
