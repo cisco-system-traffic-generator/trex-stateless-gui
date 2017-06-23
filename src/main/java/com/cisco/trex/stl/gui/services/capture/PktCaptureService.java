fc79ef669d8d92d521ae973fef80dcfefff415cc
Vyacheslav Ogai <vyacheslav.ogai@gmail.com>
2017-06-23 10:02:12 +0700

    Add stopMonitor to pktCaptureService

--- src/main/java/com/cisco/trex/stl/gui/services/capture/PktCaptureService.java
+++ src/main/java/com/cisco/trex/stl/gui/services/capture/PktCaptureService.java
@@ -5,19 +5,19 @@ import com.cisco.trex.stateless.model.TRexClientResult;
 import com.cisco.trex.stateless.model.capture.CaptureInfo;
 import com.cisco.trex.stateless.model.capture.CaptureMonitor;
 import com.cisco.trex.stateless.model.capture.CapturedPackets;
-import com.exalttech.trex.application.TrexApp;
+import com.exalttech.trex.core.ConnectionManager;
 import javafx.concurrent.ScheduledService;
 import javafx.concurrent.Task;
 import org.apache.log4j.Logger;
 
 import java.util.List;
 
-public class PktCaptureService extends ScheduledService<CapturedPackets>
+public class PktCaptureService extends ScheduledService<CapturedPackets> {
     private static Logger LOG = Logger.getLogger(PktCaptureService.class);
     
     private int currentActiveMonitorId = 0;
     
-    private TRexClient tRexClient = TrexApp.injector.getInstance(TRexClient.class);
+    private TRexClient tRexClient = ConnectionManager.getInstance().getTrexClient();
     
     @Override
     protected Task<CapturedPackets> createTask() {
@@ -42,10 +42,16 @@ public class PktCaptureService extends ScheduledService<CapturedPackets>
         TRexClientResult<CaptureMonitor> result = tRexClient.captureMonitorStart(rx, tx);
         guardNotFailed(result);
         currentActiveMonitorId = result.get().getCaptureId();
+        start();
         return true;
     }
-    
-    public CapturedPackets fetchCapturedPkts(int captureId, int chunkSize) throws PktCaptureServiceException {
+
+    synchronized public void stopMonitor() {
+        tRexClient.captureMonitorStop(currentActiveMonitorId);
+        tRexClient.captureMonitorRemove(currentActiveMonitorId);
+    }
+
+    synchronized public CapturedPackets fetchCapturedPkts(int captureId, int chunkSize) throws PktCaptureServiceException {
         TRexClientResult<CapturedPackets> result = tRexClient.captureFetchPkts(captureId, chunkSize);
         guardNotFailed(result);
         return result.get();
