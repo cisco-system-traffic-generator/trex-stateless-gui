450b3c54f321aba5b155f0a33417288b92906de8
Vyacheslav Ogai <vyacheslav.ogai@gmail.com>
2017-06-28 12:19:11 +0700

    Remove Port filter stub. Now ports populates from PortManager

--- src/main/java/com/cisco/trex/stl/gui/controllers/capture/PortFilterController.java
+++ src/main/java/com/cisco/trex/stl/gui/controllers/capture/PortFilterController.java
@@ -1,5 +1,6 @@
 package com.cisco.trex.stl.gui.controllers.capture;
 
+import com.exalttech.trex.ui.PortsManager;
 import com.exalttech.trex.util.Initialization;
 import javafx.fxml.FXML;
 import javafx.scene.layout.HBox;
@@ -7,16 +8,23 @@ import org.controlsfx.control.CheckComboBox;
 
 import java.util.List;
 
+import static java.util.stream.Collectors.toList;
+
 public class PortFilterController extends HBox {
     
     @FXML
-    private CheckComboBox rxFilter;
+    private CheckComboBox<String> rxFilter;
     
     @FXML
-    private CheckComboBox txFilter;
+    private CheckComboBox<String> txFilter;
 
     public PortFilterController() {
         Initialization.initializeFXML(this, "/fxml/pkt_capture/PortFilter.fxml");
+
+        List<String> choices = PortsManager.getInstance().getPortList().stream().map(port -> String.format("Port %s", port.getIndex())).collect(toList());
+        
+        rxFilter.getItems().addAll(choices);
+        txFilter.getItems().addAll(choices);
     }
 
     public List<Integer> getRxPorts() {
