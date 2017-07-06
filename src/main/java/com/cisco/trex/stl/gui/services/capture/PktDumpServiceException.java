2e7fe9577e88744196798410ce87a63abaeb2a4f
hedjuo <vyacheslav.ogai@gmail.com>
2017-07-06 09:18:57 +0700

    Add PktDumpServiceException and PktDumpServiceInitException.

---
+++ src/main/java/com/cisco/trex/stl/gui/services/capture/PktDumpServiceException.java
@@ -0,0 +1,7 @@
+package com.cisco.trex.stl.gui.services.capture;
+
+public class PktDumpServiceException extends Exception {
+    public PktDumpServiceException(String message) {
+        super(message);
+    }
+}
