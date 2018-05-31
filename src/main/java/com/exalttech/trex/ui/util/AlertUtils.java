d619f2d73e99962fc1ae9aeff2082cdbe338b1cb
Egor Blagov <egor.blagov@xored.com>
2018-05-31 20:06:42 +0700

    Changes in style of dialogs
    - fixed buttons ok, cancel etc. style
    - added style for contents of dialog which is created by AlertUtils (Text is still looks kinda weird)

--- src/main/java/com/exalttech/trex/ui/util/AlertUtils.java
+++ src/main/java/com/exalttech/trex/ui/util/AlertUtils.java
@@ -1,24 +1,53 @@
 package com.exalttech.trex.ui.util;
 
+import com.exalttech.trex.application.TrexApp;
 import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.scene.layout.HBox;
 import javafx.scene.text.Text;
 
 
 public class AlertUtils {
-    static public Alert construct(Alert.AlertType type, String title, String header, String msg) {
-        Text text = new Text(msg);
-        text.setWrappingWidth(350);
+    public static final int WRAPPING_WIDTH = 350;
 
-        HBox container = new HBox();
-        container.setSpacing(10);
-        container.getChildren().add(text);
+    static public Alert construct(Alert.AlertType type, String title, String header, String msg, ButtonType ... buttonTypes) {
+        Alert alert = new Alert(type, null, buttonTypes);
 
-        Alert alert = new Alert(type);
-        alert.setTitle(title);
-        alert.setHeaderText(header);
-        alert.getDialogPane().setContent(container);
+        if (msg != null) {
+            Text text = new Text(msg);
+            text.setWrappingWidth(WRAPPING_WIDTH);
+            text.getStyleClass().add("alert-text");
+            HBox container = new HBox();
+            container.getChildren().add(text);
+            alert.getDialogPane().setContent(container);
+
+        }
+
+        alert.getDialogPane().getStyleClass().add("alert-content");
+
+        if (title != null) {
+            alert.setTitle(title);
+        }
+
+        if (header != null) {
+            alert.setHeaderText(header);
+        }
+
+        alert.getDialogPane()
+                .getScene()
+                .getStylesheets()
+                .add(TrexApp.class.getResource("/styles/mainStyle.css").toExternalForm());
+
+
 
         return alert;
+    }
+
+    static public Alert construct(Alert.AlertType type, String title, String header, String msg) {
+        return construct(type, title, header, msg, ButtonType.OK);
+    }
+
+    static public Alert constructEmpty(Alert.AlertType type, ButtonType ... buttonTypes) {
+        return construct(type, null, null, null, buttonTypes);
     }
 }
